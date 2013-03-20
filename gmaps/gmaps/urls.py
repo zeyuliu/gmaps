cc7ce7ba6605ac80b99cffb6e38c4fc4f0cb918b
Andrew <zeyuliu@gmail.com>
2013-03-20 09:59:25 -0700

    url mapping for map app

--- gmaps/gmaps/urls.py
+++ gmaps/gmaps/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import patterns, include, url
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
+    url(r'^$', include('map.urls'))
     # Examples:
     # url(r'^$', 'gmaps.views.home', name='home'),
     # url(r'^gmaps/', include('gmaps.foo.urls')),
@@ -13,5 +15,5 @@ urlpatterns = patterns('',
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
 )
